e1b7e660e64ff79ff44422c845ec3d840c2910f5
Mujtaba <mujtaba51472@gmail.com>
2022-04-03 20:59:09 +0500

    movie app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React , {useEffect, useState} from 'react'
+import searchIcon from './search.svg'
+import MovieCard from './MovieCard';
+// key a9193a82
+const API_URL= 'http://www.omdbapi.com?apikey=a9193a82';
+const movie1=
+  {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 function App() {
+  const [movie, setmovie] = useState([])
+  const [searchTerm, setSearchTerm] = useState([])
+
+  console.log('first', movie)
+
+
+  const searchMovies =async (title)=>{
+    const res = await  axios(`${API_URL}&s=${title}`)
+   const data = await res.data.Search;
+   setmovie(data)
+ 
+  }
+  useEffect(()=>{
+    searchMovies("King")
+
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    
+   <h1>MovieLand</h1>
+   <div className='search'>
+     <input type="text" placeholder='search for a movie' value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} />
+     <img src={searchIcon} alt="search"   onClick={()=>{searchMovies(searchTerm)}}/>
+   </div>
+   <div className="container">
+     {
+       movie.map(movie=> ( <MovieCard movie1={movie} />))
+     }
+    
+   
+   </div>
+  
     </div>
   );
 }
